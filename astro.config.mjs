cca3b68d3c71de221cfa4adb04e4eab5d85e49b5
Ricardo Suarez <rjosesuarez@unicesar.edu.co>
2025-11-28 14:01:39 -0500

    fix: correct base path to my_portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ricarsurbusiness.github.io",
-  base: "/portfolio_rs",
+  base: "/my_portfolio",
   output: "static",
 });
