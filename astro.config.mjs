f49b534cde2ea3c8f2956345de2e28ff75353aa4
Ricardo Suarez <rjosesuarez@unicesar.edu.co>
2025-11-28 13:42:21 -0500

    feat: configure deployment on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ricarsurbusiness.github.io",
+  base: "/portfolio_rs",
+  output: "static",
+});
